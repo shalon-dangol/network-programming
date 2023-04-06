9ecb08abb2d9b496f645d1116c858bbd57d64424
Shalon Dangol <shalondangol.sd@gmail.com>
2023-04-06 07:48:12 +0545

    updated

--- ipnet/src/SocketDemo/ClientMachine.java
+++ ipnet/src/SocketDemo/ClientMachine.java
@@ -4,6 +4,10 @@
  */
 package SocketDemo;
 
+import java.io.BufferedReader;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 import javax.swing.*;
 
 /**
@@ -11,20 +15,71 @@ import javax.swing.*;
  * @author Shalon
  */
 public class ClientMachine {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel p = new JPanel();
-        JLabel lbl = new JLabel("client");
-        JTextArea t = new JTextArea(10,30);
-        JTextField mField = new JTextField(20);
-        JButton btn = new JButton("click");
-        GroupLayout layout = new GroupLayout(p);
-        layout.setVerticalGroup(layout.createSequentialGroup().addComponent(lbl).addComponent(t));
-        p.add(mField);
-        p.add(btn);
-        frame.add(p);
-        frame.setSize(500,500);
-        frame.setVisible(true);
+    public static void main(String[] args) throws IOException {
+//        JFrame frame = new JFrame();
+//        JPanel p = new JPanel();
+//        JLabel lbl = new JLabel("client");
+//        JTextArea t = new JTextArea(10,30);
+//        JTextField mField = new JTextField(20);
+//        JButton btn = new JButton("click");
+//        GroupLayout layout = new GroupLayout(p);
+//        layout.setVerticalGroup(layout.createSequentialGroup().addComponent(lbl).addComponent(t));
+//        p.add(mField);
+//        p.add(btn);
+//        frame.add(p);
+//        frame.setSize(500,500);
+//        frame.setVisible(true);
+
+          final Socket clientSocket;
+          final BufferedReader in;
+          final PrintWriter out;
+          final Scanner sc = new Scanner(System.in);
+          try{
+              clientSocket = new Socket("192.168.1.1",1234);
+              System.out.println("Client Started\n");
+              out = new PrintWriter(clientSocket.getOutputStream());
+              in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+              
+              Thread sender = new Thread (new  Runnable(){
+                  String msg;
+
+                  @Override
+                  public void run() {
+                      while(true){
+                          msg= sc.nextLine();
+                          out.println(msg);
+                          out.flush();
+                      }
+                  }
+                  
+              });
+              sender.start();
+          
+          
+          Thread receiver = new Thread(new Runnable(){
+              String msg;
+              @Override
+              public void run() {
+                 try{
+                     msg= in.readLine();
+                     while(msg!=null){
+                         System.out.println("Server: "+msg);
+                         msg= in.readLine();
+                     }
+                     System.out.println("Server out of service");
+                     out.close();
+                     clientSocket.close();
+                 }catch(IOException e){
+                     e.printStackTrace();
+                 }
+              }
+              
+          });
+          receiver.start();
+          
         
+    }catch(IOException e){
+    e.printStackTrace();
+}
     }
 }
--- ipnet/src/SocketDemo/ServerMachine.java
+++ ipnet/src/SocketDemo/ServerMachine.java
@@ -5,6 +5,9 @@
 package SocketDemo;
 
 import java.awt.*;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 import javax.swing.*;
 
 /**
@@ -12,20 +15,71 @@ import javax.swing.*;
  * @author Shalon
  */
 public class ServerMachine {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel p = new JPanel();
-        JLabel lbl = new JLabel("server");
-        JTextArea t = new JTextArea(10,30);
-        JTextField mField = new JTextField(20);
-        JButton btn = new JButton("click");
-        GroupLayout layout = new GroupLayout(p);
-        layout.setVerticalGroup(layout.createSequentialGroup().addComponent(lbl).addComponent(t));
-        p.add(mField);
-        p.add(btn);
-        frame.add(p);
-        frame.setSize(500,500);
-        frame.setVisible(true);
-        
+    public static void main(String[] args) throws IOException {
+//        JFrame frame = new JFrame();
+//        JPanel p = new JPanel();
+//        JLabel lbl = new JLabel("server");
+//        JTextArea t = new JTextArea(10,30);
+//        JTextField mField = new JTextField(20);
+//        JButton btn = new JButton("click");
+//        GroupLayout layout = new GroupLayout(p);
+//        layout.setVerticalGroup(layout.createSequentialGroup().addComponent(lbl).addComponent(t));
+//        p.add(mField);
+//        p.add(btn);
+//        frame.add(p);
+//        frame.setSize(500,500);
+//        frame.setVisible(true);
+
+          final ServerSocket  serverSocket;
+          final Socket clientSocket;
+          final BufferedReader in;
+          final PrintWriter out;
+          final Scanner sc = new Scanner(System.in);
+          
+          try{
+              serverSocket = new ServerSocket(1234);
+              clientSocket = serverSocket.accept();
+              System.out.println("server started\n");
+              out = new PrintWriter(clientSocket.getOutputStream());
+              in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+              Thread sender = new Thread(new Runnable() {
+                  String msg;
+                  @Override
+                  public void run() {
+                      while(true){
+                          msg = sc.nextLine();
+                          out.println(msg);
+                          out.flush();
+                      }
+                  }
+              });
+              sender.start();
+              
+              Thread receive = new Thread(new Runnable(){
+                  String msg;
+
+                  @Override
+                  public void run() {
+                      try{
+                          msg=in.readLine();
+                          while(msg!=null){
+                              System.out.println("Client: "+msg);
+                              msg = in.readLine();
+                              
+                          }
+                          System.out.println("Client Disconnected");
+                          out.close();
+                          serverSocket.close();
+                          
+                      }catch(IOException e){
+                          e.printStackTrace();
+                      }
+                  }
+                  
+              });
+              receive.start();
+          }catch(IOException e){
+              e.printStackTrace();
+          }
     }
 }
